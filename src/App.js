0b87497733f39b8c3c75d2d068482be7d373117d
Stefano Gazzotti <gazzottistefano@gmail.com>
2023-02-23 20:19:23 -0300

    Delete unnecesary files Refactor to introduce new helper file and reduce redundant code

--- src/App.js
+++ src/App.js
@@ -3,17 +3,7 @@ import './App.css';
 import Rol from './components/Rol';
 import Champion from './components/RolChampion';
 
-const listChampsTop = ["Aatrox", "Akshan", "Camille", "Chogath", "Darius", "DrMundo", "Gangplank", "Garen", "Gnar", "Gwen", "Illaoi", "Irelia", "Jax", "Jayce", "KSante", "Kayle", "Kennen", "Kled", "Malphite", "Maokai", "Mordekaiser", "Nasus", "Nidalee", "Olaf", "Ornn", "Pantheon", "Poppy", "Quinn", "Renekton", "Riven", "Rumble", "Sett", "Shen", "Singed", "Sion", "Teemo", "Trundle", "Tryndamere", "Urgot", "Vladimir", "Volibear", "Wukong", "Yorick"];
-
-const listChampsJg = ["Amumu", "Belveth", "Diana", "Ekko", "Elise", "Evelynn", "Fiddlesticks", "Graves", "Hecarim", "Ivern", "JarvanIV", "Karthus", "Kayn", "Khazix", "Kindred", "LeeSin", "Lillia", "MasterYi", "Maokai", "Nocturne", "Nunu", "Rammus", "RekSai", "Rengar", "Sejuani", "Shaco", "Shyvana", "Skarner", "Trundle", "Udyr", "Vi", "Viego", "volibear", "XinZhao", "Zac"];
-
-const listChampsMid = ["Ahri", "Akali", "Akshan", "Anivia", "Annie", "AurelionSol", "Azir", "Brand", "Cassiopeia", "Corki", "Diana", "Ekko", "Fizz", "Galio", "Gragas", "Heimerdinger", "Jayce", "Karma", "Karthus", "Kassadin", "Katarina", "Leblanc", "Lissandra", "Lux", "Malzahar", "Neeko", "Orianna", "Qiyana", "Ryze", "Sylas", "Syndra", "Taliyah", "Talon", "Teemo", "TwistedFate", "Veigar", "Velkoz", "Vex", "Viktor","Vladimir", "Xerath", "Yasuo", "Yone", "Zed", "Ziggs","Zoe"];
-
-const listChampsADC = ["Aphelios", "Ashe", "Caitlyn", "Draven", "Ezreal", "Jhin", "Jinx", "Kaisa", "Kalista", "KogMaw", "Lucian", "MissFortune", "Tristana", "Twitch", "Varus", "Vayne", "Xayah", "Zeri"];
-
-const listChampsSupp = ["Alistar", "Bard", "Braum", "Janna", "Karma", "Leona", "Lulu", "Lux", "Morgana", "Nami", "Nautilus", "Pyke", "Rakkan", "Rell", "Renata glasc", "Senna", "Seraphine", "Sona", "Soraka", "TahmKench", "Taric", "Thresh", "Yuumi", "Zilean", "Zyra"];
-
-const roles = ["Top", "Jg", "Mid", "ADC", "Supp"];
+const utils = require('./helper/utils.js');
  
 function App() {
   const [rol, setRol] = useState(null);
@@ -26,55 +16,24 @@ function App() {
 
   function rolChampRandom() {
     clear();
-    const randomRol = roles[Math.floor(Math.random() * roles.length)];
-    setRol(randomRol);
-
-    if (randomRol === "Top"){
-      let randomChamp = listChampsTop[Math.floor(Math.random() * listChampsTop.length)];
-      setChamp(randomChamp);
-      console.log(champ)
-    }
-    
-
-    else if (randomRol === "Jg"){
-      let randomChamp = listChampsJg[Math.floor(Math.random() * listChampsJg.length)];
-      setChamp(randomChamp);
-      console.log(champ)
-    }
-
-    else if (randomRol === "Mid"){
-      let randomChamp = listChampsMid[Math.floor(Math.random() * listChampsMid.length)];
-      setChamp(randomChamp);
-      console.log(champ)
-    }
-
-    else if (randomRol === "ADC"){
-      let randomChamp = listChampsADC[Math.floor(Math.random() * listChampsADC.length)];
-      setChamp(randomChamp);
-      console.log(champ)
-    }
-
-    else if (randomRol === "Supp"){
-      let randomChamp = listChampsSupp[Math.floor(Math.random() * listChampsSupp.length)];
-      setChamp(randomChamp);
-      console.log(champ)
-    }
+    const randomRol = utils.selectRandomRol();
+    setRol(utils.capitalize(randomRol));
+    setChamp(utils.selectChamp(randomRol))
   }
 
   function rolRandom(){
     clear();
-    const randomRol = roles[Math.floor(Math.random() * roles.length)];
-    setRol(randomRol);
-
+    const randomRol = utils.selectRandomRol();
+    setRol(utils.capitalize(randomRol));
   }
 
   function rolFiveChamps(){
     clear();
-    setChampTop(listChampsTop[Math.floor(Math.random() * listChampsTop.length)])
-    setChampJg(listChampsJg[Math.floor(Math.random() * listChampsJg.length)])
-    setChampMid(listChampsMid[Math.floor(Math.random() * listChampsMid.length)])
-    setChampADC(listChampsADC[Math.floor(Math.random() * listChampsADC.length)])
-    setChampSupp(listChampsSupp[Math.floor(Math.random() * listChampsSupp.length)])
+    setChampTop(utils.selectChamp("top"))
+    setChampJg(utils.selectChamp("jungler"))
+    setChampMid(utils.selectChamp("mid"))
+    setChampADC(utils.selectChamp("adc"))
+    setChampSupp(utils.selectChamp("support"))
   }
 
   function clear(){
---
+++ src/helper/utils.js
@@ -0,0 +1,29 @@
+const roles = ["top", "jungler", "mid", "adc", "support"];
+
+const Champs = {
+  top: ["Aatrox", "Akshan", "Camille", "Chogath", "Darius", "DrMundo", "Gangplank", "Garen", "Gnar", "Gwen", "Illaoi", "Irelia", "Jax", "Jayce", "KSante", "Kayle", "Kennen", "Kled", "Malphite", "Maokai", "Mordekaiser", "Nasus", "Nidalee", "Olaf", "Ornn", "Pantheon", "Poppy", "Quinn", "Renekton", "Riven", "Rumble", "Sett", "Shen", "Singed", "Sion", "Teemo", "Trundle", "Tryndamere", "Urgot", "Vladimir", "Volibear", "Wukong", "Yorick"],
+  jungler: ["Amumu", "Belveth", "Diana", "Ekko", "Elise", "Evelynn", "Fiddlesticks", "Graves", "Hecarim", "Ivern", "JarvanIV", "Karthus", "Kayn", "Khazix", "Kindred", "LeeSin", "Lillia", "MasterYi", "Maokai", "Nocturne", "Nunu", "Rammus", "RekSai", "Rengar", "Sejuani", "Shaco", "Shyvana", "Skarner", "Trundle", "Udyr", "Vi", "Viego", "volibear", "XinZhao", "Zac"],
+  mid: ["Ahri", "Akali", "Akshan", "Anivia", "Annie", "AurelionSol", "Azir", "Brand", "Cassiopeia", "Corki", "Diana", "Ekko", "Fizz", "Galio", "Gragas", "Heimerdinger", "Jayce", "Karma", "Karthus", "Kassadin", "Katarina", "Leblanc", "Lissandra", "Lux", "Malzahar", "Neeko", "Orianna", "Qiyana", "Ryze", "Sylas", "Syndra", "Taliyah", "Talon", "Teemo", "TwistedFate", "Veigar", "Velkoz", "Vex", "Viktor","Vladimir", "Xerath", "Yasuo", "Yone", "Zed", "Ziggs","Zoe"],
+  adc: ["Aphelios", "Ashe", "Caitlyn", "Draven", "Ezreal", "Jhin", "Jinx", "Kaisa", "Kalista", "KogMaw", "Lucian", "MissFortune", "Tristana", "Twitch", "Varus", "Vayne", "Xayah", "Zeri"],
+  support: ["Alistar", "Bard", "Braum", "Janna", "Karma", "Leona", "Lulu", "Lux", "Morgana", "Nami", "Nautilus", "Pyke", "Rakkan", "Rell", "Renata glasc", "Senna", "Seraphine", "Sona", "Soraka", "TahmKench", "Taric", "Thresh", "Yuumi", "Zilean", "Zyra"]
+};
+
+function selectRandomRol(){
+  return roles[Math.floor(Math.random() * roles.length)];
+}
+
+function selectChamp(roles) {
+  return Champs[roles][Math.floor(Math.random() * Champs[roles].length)];
+}
+
+function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+module.exports = {
+  roles,
+  Champs,
+  selectRandomRol,
+  selectChamp,
+  capitalize
+};
