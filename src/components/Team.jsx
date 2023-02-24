961c38af77141026c5eccd829f0c4c6b36902670
Stefano Gazzotti <gazzottistefano@gmail.com>
2023-02-23 21:47:33 -0300

    Fix rol naming

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -6,10 +6,10 @@ export default function Team({champions}) {
   return (
     <div className='container-five-champs'>
       <Champion rol="Top" champ={champions.top} quantity="5" />
-      <Champion rol="Jg" champ={champions.jungler} quantity="5" />
+      <Champion rol="Jungler" champ={champions.jungler} quantity="5" />
       <Champion rol="Mid" champ={champions.mid} quantity="5" />
-      <Champion rol="ADC" champ={champions.adc} quantity="5" />
-      <Champion rol="Supp" champ={champions.support} quantity="5" />
+      <Champion rol="Adc" champ={champions.adc} quantity="5" />
+      <Champion rol="Support" champ={champions.support} quantity="5" />
     </div>
   )
 }
